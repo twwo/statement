19f4fbc452a082661e25a0e34be7665ba9208a1a
LISH5 <727904507@qq.com>
2020-08-27 15:15:01 +0800

    [Statement][Shao] refactor: delete comment

--- src/statement.js
+++ src/statement.js
@@ -36,23 +36,6 @@ function statement (invoice, plays) {
   for (let performance of invoice.performances) {
     const play = plays[performance.playID];
     let thisAmount = calculateAmount(play.type, performance);
-//    switch (play.type) {
-//      case 'tragedy':
-//        thisAmount = 40000;
-//        if (performance.audience > 30) {
-//          thisAmount += 1000 * (performance.audience - 30);
-//        }
-//        break;
-//      case 'comedy':
-//        thisAmount = 30000;
-//        if (performance.audience > 20) {
-//          thisAmount += 10000 + 500 * (performance.audience - 20);
-//        }
-//        thisAmount += 300 * performance.audience;
-//        break;
-//      default:
-//        throw new Error(`unknown type: ${play.type}`);
-//    }
     // add volume credits
     volumeCredits += Math.max(performance.audience - 30, 0);
     // add extra credit for every ten comedy attendees
